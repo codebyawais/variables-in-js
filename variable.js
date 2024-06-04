ec93d5b2274739ded1fea495ac5e23bb2f5a6d5d
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-04 15:35:42 +0500

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+const accountid ="awais rafiq";
+// accountid=2;
+// console.log(accountid);
+
+
+let accountemail ="awaisrafiq@gmail.com";
+// accountemail="webbyawais"
+// accountemail="codebyawais"
+// console.log(accountemail);
+
+var accountpassword ="123455@ee"
+// accountpassword="awais1122"
+// console.log(accountpassword);
+
+let state;
+
+
+console.table([accountid, accountemail, accountpassword, state])
